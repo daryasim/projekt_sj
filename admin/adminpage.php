226a44b5022a002e41fe926f059f67876ee10db0
daryasim <dashasimakova8@gmail.com>
2023-05-10 22:08:19 +0200

    fix

---
+++ admin/adminpage.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+echo "Admin!!!";
+
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Admin Panel</title>
+
+</head>
+<body>
+    <div>
+      <a href = "../logout.php">Log out</a>
+    </div>
+
+
+</body>
+</html>
